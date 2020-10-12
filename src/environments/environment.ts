5600c5a496c719c31c04c004d293c86229416f10
eliyahu porush <eliyahuporush@gmail.com>
2020-10-13 01:19:52 +0300

    ..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyCFHhL5pC5_ZeVTaq8bQgfCSNcUOjPvNaE",
-    authDomain: "slack-b0c55.firebaseapp.com",
-    databaseURL: "https://slack-b0c55.firebaseio.com",
-    projectId: "slack-b0c55",
-    storageBucket: "slack-b0c55.appspot.com",
-    messagingSenderId: "21660727028",
-    appId: "1:21660727028:web:f9defd569afe47701510a1",
-    measurementId: "G-HCW2LQRTSZ"
+  firebaseConfig: {
+    apiKey: "AIzaSyDFg_hFTiB96URCXQFQNEdA3pGnD3QVvsA",
+    authDomain: "workchat-6060.firebaseapp.com",
+    databaseURL: "https://workchat-6060.firebaseio.com",
+    projectId: "workchat-6060",
+    storageBucket: "workchat-6060.appspot.com",
+    messagingSenderId: "537514835864",
+    appId: "1:537514835864:web:4fd913611fc2ed46c7f5ed",
+    measurementId: "G-18KX3E4Q72"
   }
 };
 
